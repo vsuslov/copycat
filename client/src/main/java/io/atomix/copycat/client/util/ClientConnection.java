152ee962bdaaef7346dc00915021e24a461e9498
Jordan Halterman <jordan.halterman@gmail.com>
2017-05-15 09:22:06 -0700

    Checkpoint.

--- client/src/main/java/io/atomix/copycat/client/util/ClientConnection.java
+++ client/src/main/java/io/atomix/copycat/client/util/ClientConnection.java
@@ -15,12 +15,11 @@
  */
 package io.atomix.copycat.client.util;
 
-import io.atomix.catalyst.concurrent.Listener;
 import io.atomix.catalyst.transport.Address;
-import io.atomix.catalyst.transport.Client;
 import io.atomix.catalyst.transport.Connection;
 import io.atomix.catalyst.transport.TransportException;
 import io.atomix.catalyst.util.Assert;
+import io.atomix.copycat.client.session.impl.CopycatSessionState;
 import io.atomix.copycat.error.CopycatError;
 import io.atomix.copycat.protocol.ConnectRequest;
 import io.atomix.copycat.protocol.ConnectResponse;
@@ -45,19 +44,19 @@ import java.util.function.Function;
  *
  * @author <a href="http://github.com/kuujo>Jordan Halterman</a>
  */
-public class ClientConnection implements Connection {
+public class ClientConnection {
   private static final Logger LOGGER = LoggerFactory.getLogger(ClientConnection.class);
-  private final String id;
-  private final Client client;
+  private final CopycatSessionState state;
+  private final ConnectionManager connections;
   private final AddressSelector selector;
   private CompletableFuture<Connection> connectFuture;
-  private final Map<Class<?>, Function> handlers = new ConcurrentHashMap<>();
+  private final Map<String, Function> handlers = new ConcurrentHashMap<>();
   private Connection connection;
   private boolean open = true;
 
-  public ClientConnection(String id, Client client, AddressSelector selector) {
-    this.id = Assert.notNull(id, "id");
-    this.client = Assert.notNull(client, "client");
+  public ClientConnection(CopycatSessionState state, ConnectionManager connections, AddressSelector selector) {
+    this.state = Assert.notNull(state, "state");
+    this.connections = Assert.notNull(connections, "connections");
     this.selector = Assert.notNull(selector, "selector");
   }
 
@@ -101,17 +100,31 @@ public class ClientConnection implements Connection {
     return this;
   }
 
-  @Override
-  public CompletableFuture<Void> send(Object request) {
+  /**
+   * Sends a request to the cluster.
+   *
+   * @param type The request type.
+   * @param request The request to send.
+   * @return A completable future to be completed with the response.
+   */
+  public CompletableFuture<Void> send(String type, Object request) {
     CompletableFuture<Void> future = new CompletableFuture<>();
-    sendRequest((Request) request, (r, c) -> c.send(r), future);
+    sendRequest((Request) request, (r, c) -> c.send(type, r), future);
     return future;
   }
 
-  @Override
-  public <T, U> CompletableFuture<U> sendAndReceive(T request) {
+  /**
+   * Sends a request to the cluster and awaits a response.
+   *
+   * @param type The request type.
+   * @param request The request to send.
+   * @param <T> The request type.
+   * @param <U> The response type.
+   * @return A completable future to be completed with the response.
+   */
+  public <T, U> CompletableFuture<U> sendAndReceive(String type, T request) {
     CompletableFuture<U> future = new CompletableFuture<>();
-    sendRequest((Request) request, (r, c) -> c.sendAndReceive(r), future);
+    sendRequest((Request) request, (r, c) -> c.sendAndReceive(type, r), future);
     return future;
   }
 
@@ -131,7 +144,7 @@ public class ClientConnection implements Connection {
     if (open) {
       if (error == null) {
         if (connection != null) {
-          LOGGER.trace("{} - Sending {}", id, request);
+          LOGGER.trace("{} - Sending {}", state.getSessionString(), request);
           sender.apply(request, connection).whenComplete((r, e) -> {
             if (e != null || r != null) {
               handleResponse(request, sender, connection, (Response) r, e, future);
@@ -143,7 +156,7 @@ public class ClientConnection implements Connection {
           future.completeExceptionally(new ConnectException("Failed to connect to the cluster"));
         }
       } else {
-        LOGGER.trace("{} - Resending {}: {}", id, request, error);
+        LOGGER.trace("{} - Resending {}: {}", state.getSessionString(), request, error);
         resendRequest(error, request, sender, connection, future);
       }
     }
@@ -156,9 +169,8 @@ public class ClientConnection implements Connection {
   private <T extends Request> void resendRequest(Throwable cause, T request, BiFunction sender, Connection connection, CompletableFuture future) {
     // If the connection has not changed, reset it and connect to the next server.
     if (this.connection == connection) {
-      LOGGER.trace("{} - Resetting connection. Reason: {}", id, cause);
+      LOGGER.trace("{} - Resetting connection. Reason: {}", state.getSessionString(), cause);
       this.connection = null;
-      connection.close();
     }
 
     // Create a new connection and resend the request. This will force retries to piggyback on any existing
@@ -177,9 +189,11 @@ public class ClientConnection implements Connection {
           || response.error() == CopycatError.Type.COMMAND_ERROR
           || response.error() == CopycatError.Type.QUERY_ERROR
           || response.error() == CopycatError.Type.APPLICATION_ERROR
+          || response.error() == CopycatError.Type.UNKNOWN_CLIENT_ERROR
           || response.error() == CopycatError.Type.UNKNOWN_SESSION_ERROR
+          || response.error() == CopycatError.Type.UNKNOWN_STATE_MACHINE_ERROR
           || response.error() == CopycatError.Type.INTERNAL_ERROR) {
-          LOGGER.trace("{} - Received {}", id, response);
+          LOGGER.trace("{} - Received {}", state.getSessionString(), response);
           future.complete(response);
         } else {
           resendRequest(response.error().createException(), request, sender, connection, future);
@@ -187,7 +201,7 @@ public class ClientConnection implements Connection {
       } else if (error instanceof ConnectException || error instanceof TimeoutException || error instanceof TransportException || error instanceof ClosedChannelException) {
         resendRequest(error, request, sender, connection, future);
       } else {
-        LOGGER.debug("{} - {} failed! Reason: {}", id, request, error);
+        LOGGER.debug("{} - {} failed! Reason: {}", state.getSessionString(), request, error);
         future.completeExceptionally(error);
       }
     }
@@ -207,9 +221,7 @@ public class ClientConnection implements Connection {
       future.whenComplete((r, e) -> this.connectFuture = null);
       this.connectFuture = future;
 
-      Connection oldConnection = this.connection;
       this.connection = null;
-      oldConnection.close();
       connect(future);
       return future;
     }
@@ -237,12 +249,12 @@ public class ClientConnection implements Connection {
    */
   private void connect(CompletableFuture<Connection> future) {
     if (!selector.hasNext()) {
-      LOGGER.debug("{} - Failed to connect to the cluster", id);
+      LOGGER.debug("{} - Failed to connect to the cluster", state.getSessionString());
       future.complete(null);
     } else {
       Address address = selector.next();
-      LOGGER.debug("{} - Connecting to {}", id, address);
-      client.connect(address).whenComplete((c, e) -> handleConnection(address, c, e, future));
+      LOGGER.debug("{} - Connecting to {}", state.getSessionString(), address);
+      connections.getConnection(address).whenComplete((c, e) -> handleConnection(address, c, e, future));
     }
   }
 
@@ -254,7 +266,7 @@ public class ClientConnection implements Connection {
       if (error == null) {
         setupConnection(address, connection, future);
       } else {
-        LOGGER.debug("{} - Failed to connect! Reason: {}", id, error);
+        LOGGER.debug("{} - Failed to connect! Reason: {}", state.getSessionString(), error);
         connect(future);
       }
     }
@@ -265,35 +277,40 @@ public class ClientConnection implements Connection {
    */
   @SuppressWarnings("unchecked")
   private void setupConnection(Address address, Connection connection, CompletableFuture<Connection> future) {
-    LOGGER.debug("{} - Setting up connection to {}", id, address);
+    LOGGER.debug("{} - Setting up connection to {}", state.getSessionString(), address);
 
     this.connection = connection;
 
     connection.onClose(c -> {
       if (c.equals(this.connection)) {
-        LOGGER.debug("{} - Connection closed", id);
+        LOGGER.debug("{} - Connection closed", state.getSessionString());
         this.connection = null;
       }
     });
     connection.onException(c -> {
       if (c.equals(this.connection)) {
-        LOGGER.debug("{} - Connection lost", id);
+        LOGGER.debug("{} - Connection lost", state.getSessionString());
         this.connection = null;
       }
     });
 
-    for (Map.Entry<Class<?>, Function> entry : handlers.entrySet()) {
-      connection.handler(entry.getKey(), entry.getValue());
+    for (Map.Entry<String, Function> entry : handlers.entrySet()) {
+      connection.registerHandler(entry.getKey(), entry.getValue());
     }
 
     // When we first connect to a new server, first send a ConnectRequest to the server to establish
     // the connection with the server-side state machine.
-    ConnectRequest request = ConnectRequest.builder()
-      .withClientId(id)
-      .build();
+    if (state.getSessionId() > 0) {
+      ConnectRequest request = ConnectRequest.builder()
+        .withSession(state.getSessionId())
+        .withConnection(state.nextConnection())
+        .build();
 
-    LOGGER.trace("{} - Sending {}", id, request);
-    connection.<ConnectRequest, ConnectResponse>sendAndReceive(request).whenComplete((r, e) -> handleConnectResponse(r, e, future));
+      LOGGER.trace("{} - Sending {}", state.getSessionString(), request);
+      connection.<ConnectRequest, ConnectResponse>sendAndReceive(ConnectRequest.NAME, request).whenComplete((r, e) -> handleConnectResponse(r, e, future));
+    } else {
+      future.complete(connection);
+    }
   }
 
   /**
@@ -302,7 +319,7 @@ public class ClientConnection implements Connection {
   private void handleConnectResponse(ConnectResponse response, Throwable error, CompletableFuture<Connection> future) {
     if (open) {
       if (error == null) {
-        LOGGER.trace("{} - Received {}", id, response);
+        LOGGER.trace("{} - Received {}", state.getSessionString(), response);
         // If the connection was successfully created, immediately send a keep-alive request
         // to the server to ensure we maintain our session and get an updated list of server addresses.
         if (response.status() == Response.Status.OK) {
@@ -312,41 +329,51 @@ public class ClientConnection implements Connection {
           connect(future);
         }
       } else {
-        LOGGER.debug("{} - Failed to connect! Reason: {}", id, error);
+        LOGGER.debug("{} - Failed to connect! Reason: {}", state.getSessionString(), error);
         connect(future);
       }
     }
   }
 
-  @Override
-  public <T, U> Connection handler(Class<T> type, Consumer<T> handler) {
-    return handler(type, r -> {
-      handler.accept(r);
+  /**
+   * Registers a handler for the given message type.
+   *
+   * @param type The message type for which to register the handler.
+   * @param handler The handler to register.
+   * @param <T> The handler type.
+   * @return The client connection.
+   */
+  @SuppressWarnings("unchecked")
+  public <T> ClientConnection registerHandler(String type, Consumer<T> handler) {
+    return registerHandler(type, r -> {
+      handler.accept((T) r);
       return null;
     });
   }
 
-  @Override
-  public <T, U> Connection handler(Class<T> type, Function<T, CompletableFuture<U>> handler) {
+  /**
+   * Registers a handler for the given message type.
+   *
+   * @param type The message type for which to register the handler.
+   * @param handler The handler to register.
+   * @param <T> The handler type.
+   * @param <U> The response type.
+   * @return The client connection.
+   */
+  public <T, U> ClientConnection registerHandler(String type, Function<T, CompletableFuture<U>> handler) {
     Assert.notNull(type, "type");
     Assert.notNull(handler, "handler");
     handlers.put(type, handler);
     if (connection != null)
-      connection.handler(type, handler);
+      connection.registerHandler(type, handler);
     return this;
   }
 
-  @Override
-  public Listener<Throwable> onException(Consumer<Throwable> listener) {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public Listener<Connection> onClose(Consumer<Connection> listener) {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
+  /**
+   * Closes the connection.
+   *
+   * @return A completable future to be completed once the connection is closed.
+   */
   public CompletableFuture<Void> close() {
     open = false;
     return CompletableFuture.completedFuture(null);
